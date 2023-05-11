c6b2dc7da33ca405e7ec40491d9128a268c47e5e
Jesse Tech <115389742+Jessyfranky@users.noreply.github.com>
2023-05-11 13:28:30 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { Component } from 'react'
+import './index.css';
+import TodoList from './TodoList';
+import './index.css';
+
+
+
+
+class App extends Component {
+    render(){
+        return (
+            <TodoList />
+        )
+    }
+}
+export default App
